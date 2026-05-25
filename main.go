1c6b73179c385654e52b1d15d7bdc83a463acbc1
duangkamolsa-design <duangkamol.sa@bitnance.com>
2026-05-25 18:40:13 +0700

    Update main.go
    handle error

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
 
-func nmber(prompt string) float64 {
+func number(prompt string) float64 {
 	var num float64
 
 	fmt.Print(prompt)
@@ -20,22 +21,22 @@ func operator() string {
 	return operator
 }
 
-func calculate(num1 float64, operator string, num2 float64) (float64, string) {
+func calculate(num1 float64, operator string, num2 float64) (float64, error) {
 	if operator == "+" {
-		return num1 + num2, ""
+		return num1 + num2, nil
 	} else if operator == "-" {
-		return num1 - num2, ""
+		return num1 - num2, nil
 	} else if operator == "*" {
-		return num1 * num2, ""
+		return num1 * num2, nil
 	} else if operator == "/" {
 		if num2 == 0 {
-			return 0, "หาร 0 ไม่ได้"
+			return 0, errors.New("หาร 0 ไม่ได้")
 		}
 
-		return num1 / num2, ""
+		return num1 / num2, nil
 
 	} else {
-		return 0, "เครื่องหมายไม่ถูกต้อง"
+		return 0, errors.New("เครื่องหมายไม่ถูกต้อง")
 	}
 }
 
@@ -48,10 +49,10 @@ func display(result float64, err string) {
 }
 
 func main() {
-	num1 := nmber("กรอกเลขตัวที่ 1: ")
+	num1 := number("กรอกเลขตัวที่ 1: ")
 	operator := operator()
-	num2 := nmber("กรอกเลขตัวที่ 2: ")
+	num2 := number("กรอกเลขตัวที่ 2: ")
 
 	result, err := calculate(num1, operator, num2)
-	display(result, err)
+	display(result, err.Error())
 }
